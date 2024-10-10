bb42bb8d079df36f7db0b754ce45662c5a3eca71
Santiago Eguidazu <eguidazu.san@gmail.com>
2024-10-09 23:38:39 -0300

    primer commit mio: contiene coleccion de usuarios y productos basico

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,25 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "script-src": ["https://cdnjs.cloudflare.com"],
+          "media-src": ["https://cdnjs.cloudflare.com"],
+          "img-src": ["https://cdnjs.cloudflare.com"],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
